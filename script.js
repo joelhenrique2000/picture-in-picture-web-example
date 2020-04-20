44eccadf209a75ae22fef174c0cfbac354c97f34
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-04-20 00:30:02 -0300

    logs in console.log

--- script.js
+++ script.js
@@ -5,12 +5,41 @@ const logBoxElement = document.getElementById('log')
 buttonToggleElement.addEventListener("click", async function(event) {
   
   try {
-    if (videoElement !== document.pictureInPictureElement)
+    if (videoElement !== document.pictureInPictureElement) {
+      console.log('Picture-in-Picture actived')
+      buttonToggleElement.disabled = true;
       await videoElement.requestPictureInPicture();
-    else
+      buttonToggleElement.disabled = false;
+    } else {
+      console.log('Picture-in-Picture desabled')
       await document.exitPictureInPicture();
+    }
   } catch(error) {
+    console.log('Picture-in-Picture error ', error)
   } finally {
+    buttonToggleElement.disabled = false;
   }
 
 })
+
+// Listening to picture-in-picture when it is activated
+video.addEventListener('enterpictureinpicture', function(event) {
+  console.log('actived Picture-in-Picture');
+
+  pictureInPictureWindow = event.pictureInPictureWindow;
+  console.log(`size: ${pictureInPictureWindow.width}x${pictureInPictureWindow.height}`);
+
+  pictureInPictureWindow.addEventListener('resize', onPipWindowResize);
+});
+
+// Listening to picture-in-picture when it is desabled
+video.addEventListener('leavepictureinpicture', function(event) {
+  console.log('> desabled Picture-in-Picture');
+
+  pictureInPictureWindow.removeEventListener('resize', onPipWindowResize);
+});
+
+// Listening to picture-in-picture when it is resized
+function onPipWindowResize(event) {
+  console.log(`size: ${pictureInPictureWindow.width}x${pictureInPictureWindow.height}`);
+}
